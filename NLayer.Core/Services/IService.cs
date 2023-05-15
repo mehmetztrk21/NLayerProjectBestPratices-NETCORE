761a1df28354fb80ba5ee58e00436be0045d5f03
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-05-15 16:13:36 +0300

    IService

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -5,22 +5,19 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NLayer.Core.Repositories
+namespace NLayer.Core.Services
 {
-    public interface IGenericRepository<T> where T : class
+    public interface IService<T> where T : class
     {
-        Task<T> GetById(int id);
-        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
 
-        //productRepository.where(x=>x.id>5).OrderBy.ToListAsync();
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> expression);
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task<T> AddAsync(T entity);
-
         Task AddRangeAsync(IEnumerable<T> entities);
-
-        void Update(T entity);
-        void Remove(T entity);
-        void RemoveRange(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
     }
 }

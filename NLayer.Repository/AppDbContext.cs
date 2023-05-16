5d9fa1f0a6eced19ee553903aa71706b980cfe6c
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-05-16 11:49:40 +0300

    DbContext

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+        
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+        //var p = new Product() { ProductFeature = new ProductFeature() };  böyle de productFeature üzerinde işlemler yapılabilir.
+
+    }
+}

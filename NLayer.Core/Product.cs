01de363d8e02c628d8b5e6f032d7967aca9b8547
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-05-16 11:37:39 +0300

    .

--- NLayer.Core/Product.cs
+++ NLayer.Core/Product.cs
@@ -8,7 +8,7 @@ namespace NLayer.Core
 {
     public class Product:BaseEntity
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }

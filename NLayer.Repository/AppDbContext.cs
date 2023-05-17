5f5a9916bfa02e86f2de396597398eebac68e92b
mehmetztrk21 <mehmet.ozturk@dincerlojistik.com>
2023-05-17 11:46:07 +0300

    configruations

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core;
+using NLayer.Repository.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +20,14 @@ namespace NLayer.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
         //var p = new Product() { ProductFeature = new ProductFeature() };  böyle de productFeature üzerinde işlemler yapılabilir.
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //çalışmadaki bütün configuration dosyalarını yap demek.
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());//veya böyle tek tek bütün config dosyalarını ekleyebiliriz.
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
